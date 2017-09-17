0318b115813000615bcba3ed047cd2d94358b83d
stsdc <s.dac@yandex.ru>
2017-09-17 17:24:26 +0200

    get hostname

--- aruco.py
+++ aruco.py
@@ -11,7 +11,7 @@ while(True):
     #print(frame.shape) #480x640
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_50)
     parameters =  aruco.DetectorParameters_create()
 
     #print(parameters)
@@ -28,14 +28,14 @@ while(True):
     # my problem was that the cellphone put black all around it. The alrogithm
     # depends very much upon finding rectangular black blobs
 
-    # gray = aruco.drawDetectedMarkers(gray, corners, ids)
+    gray = aruco.drawDetectedMarkers(gray, corners, ids)
 
     #print(rejectedImgPoints)
     # Display the resulting frame
-    # cv2.imshow('frame',gray)
-    # if cv2.waitKey(1) & 0xFF == ord('q'):
-    #     break
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 # When everything done, release the capture
 cap.release()
-# cv2.destroyAllWindows()
+cv2.destroyAllWindows()
