bd04ecf375c78fb61137eaae7c01543f56b52421
stsdc <s.dac@yandex.ru>
2017-09-18 16:39:11 +0200

    server threading

--- server/__init__.py
+++ server/__init__.py
@@ -3,15 +3,18 @@ from log import logger
 
 logger.debug("Initializing Main Unit")
 
-try:
-    server = Server("", 9000)
-    while True:
-        server.accept()
-        data = server.recv()
-        # shortcut: data = server.accept().recv()
-        server.send({'status': 'ok'})
-except (TypeError, ValueError), e:
-    logger.exception("Unexpected exception: ", e)
-finally:
-    logger.info("Shutting down")
-    server.close()
+# server = Server("", 8888)
+# try:
+#     while True:
+#         server.accept()
+#         data = server.recv()
+#         # shortcut: data = server.accept().recv()
+#         server.send({'status': 'ok'})
+# except (TypeError, ValueError), e:
+#     logger.exception("Unexpected exception: ", e)
+# finally:
+#     logger.info("Shutting down")
+#     server.close()
+
+s = Server()
+s.run()
--- server/socketserver.py
+++ server/socketserver.py
@@ -1,87 +1,104 @@
-import multiprocessing
+import select
 import socket
 import sys
+import threading
+import json
 
-from log import logger
+class Server:
+    def __init__(self):
+        self.host = ''
+        self.port = 50000
+        self.backlog = 2
+        self.size = 1024
+        self.server = None
+        self.threads = []
 
+    def open_socket(self):
+        try:
+            self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.server.bind((self.host,self.port))
+            self.server.listen(self.backlog)
+        except socket.error, (value,message):
+            if self.server:
+                self.server.close()
+            print "Could not open socket: " + message
+            sys.exit(1)
 
-def handle(connection, address):
+    def run(self):
+        self.open_socket()
+        input = [self.server,sys.stdin]
+        running = 1
+        while running:
+            inputready,outputready,exceptready = select.select(input,[],[])
+
+            for s in inputready:
+
+                if s == self.server:
+                    # handle the server socket
+                    c = Client(self.server.accept())
+                    c.start()
+                    self.threads.append(c)
+
+                elif s == sys.stdin:
+                    # handle standard input
+                    junk = sys.stdin.readline()
+                    running = 0
+
+        # close all threads
+
+        self.server.close()
+        for c in self.threads:
+            c.join()
+
+class Client(threading.Thread):
+    def __init__(self,(client,address)):
+        threading.Thread.__init__(self)
+        self.client = client
+        self.address = address
+        self.size = 1024
+
+    def run(self):
+        running = 1
+        while running:
+            data = _recv(self.client)
+            print data
+            if data:
+                _send(self.client, data)
+            else:
+                self.client.close()
+                running = 0
+
+def _send(socket, data):
     try:
-        logger.debug("Connected %r at %r", connection, address)
-        while True:
-            data = connection.recv(1024)
-            if data == "":
-                logger.debug("Socket closed remotely")
-                break
-            logger.debug("Received data %r", data)
-            connection.sendall(data)
-            logger.debug("Sent data")
-    except:
-        logger.exception("Problem handling request")
-    finally:
-        logger.debug("Closing socket")
-        connection.close()
+        serialized = json.dumps(data)
+    except (TypeError, ValueError) as e:
+        raise Exception('You can only send JSON-serializable data')
+    # send the length of the serialized data first
+    d = '%d\n' % len(serialized)
+    socket.send(d.encode('utf-8'))
+    # send the serialized data
+    socket.sendall(serialized.encode('utf-8'))
 
 
-class Server(object):
-    client = None
+def _recv(socket):
+    # read the length of the data, letter by letter until we reach EOL
+    length_str = ''
+    char = socket.recv(1).decode('utf-8')
 
-    def __init__(self, host, port):
-        self.socket = socket.socket()
-        self.socket.bind((host, port))
-        # Listen for 2 connections
-        self.socket.listen(2)
-        logger.info("Listening...")
+    while char != '\n':
+        length_str += char
+        char = socket.recv(1).decode('utf-8')
 
-    def __del__(self):
-        self.close()
-
-    def accept(self):
-        if self.client:
-            logger.info("Client is already connected, disconnecting...")
-            self.client.close()
-        self.client, self.client_addr = self.socket.accept()
-        return self
-
-    def send(self, data):
-        if not self.client:
-            logger.exception('Cannot send data, no client is connected')
-        try:
-            serialized = json.dumps(data)
-        except (TypeError, ValueError), e:
-            logger.exception('You can only send JSON-serializable data')
-        # send the length of the serialized data first
-        socket.send('%d\n' % len(serialized))
-        # send the serialized data
-        socket.sendall(serialized)
-        return self
-
-    def recv(self):
-        if not self.client:
-            logger.exception('Cannot receive data, no client is connected')
-        length_str = ''
-        char = self.socket.recv(1)
-        while char != '\n':
-            length_str += char
-            char = self.socket.recv(1)
-        total = int(length_str)
-        # use a memoryview to receive the data chunk by chunk efficiently
-        view = memoryview(bytearray(total))
-        next_offset = 0
-        while total - next_offset > 0:
-            recv_size = self.socket.recv_into(view[next_offset:],
-                                              total - next_offset)
-            next_offset += recv_size
-        try:
-            deserialized = json.loads(view.tobytes())
-        except (TypeError, ValueError), e:
-            logger.exception('Data received was not in JSON format', e)
-        return deserialized
-
-    def close(self):
-        if self.client:
-            self.client.close()
-            self.client = None
-        if self.socket:
-            self.socket.close()
-            self.socket = None
+    total = int(length_str)
+    # use a memoryview to receive the data chunk by chunk efficiently
+    view = memoryview(bytearray(total))
+    next_offset = 0
+    while total - next_offset > 0:
+        recv_size = socket.recv_into(view[next_offset:], total - next_offset)
+        next_offset += recv_size
+    try:
+        view = view.tobytes()
+        deserialized = json.loads(view.decode('utf-8'))
+    except (TypeError, ValueError) as e:
+        raise Exception('Data received was not in JSON format')
+    return deserialized
