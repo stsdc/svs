5d4b6fe031665bda31858aa822786a91bb029cbb
stsdc <s.dac@yandex.ru>
2017-09-28 00:45:38 +0200

    😄

--- svs.py
+++ svs.py
@@ -1,12 +1,16 @@
 import socket
-import log
+from log import logger
 from cron import Cron
+
+logger.info("-------------------------------------")
+logger.info("|  *   Synergia Vision System    *  |")
+logger.info("-------------------------------------")
 
 Cron().check()
 
-# if (socket.gethostname() == 'main_unit'):
-#     from server import Server
-#     Server()
-# else:
-#     from client import Client
-#     Client()
+if (socket.gethostname() == 'main_unit'):
+    from server import Server
+    Server()
+else:
+    from client import Client
+    Client()
