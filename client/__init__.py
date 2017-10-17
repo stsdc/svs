42ddbcbc089f38396c965d2b55739b1d9365d24f
stsdc <s.dac@yandex.ru>
2017-10-18 00:06:51 +0200

    trying to figure out, why capture doesn't work

--- client/__init__.py
+++ client/__init__.py
@@ -17,18 +17,22 @@ class Client(object):
 
     def __init__(self):
         logger.debug("Initializing %s", socket.gethostname())
-        Cron().check()
-        while not Network().connect():
-            sleep(10)
-            continue
+        # Cron().check()
+        # while not Network().connect():
+        #     sleep(10)
+        #     continue
 
         self.marker_detector = MarkerDetector()
-        self.socket_client = SocketClient("10.0.0.1", 50000)
+        
+        # self.socket_client = SocketClient("10.0.0.1", 50000)
 
-    def make_dict(self, marker):
+    def make_dict(self, ids, rotations, translations):
         # that's a huge mess. It should handle multiple markers.
         # marker is a tuple
         # isApple = True if fruit == 'Apple' else False
+        for id in ids:
+            print id
+
         values = [[], [], []]
         if marker[0] is not None:
             values = [
@@ -45,23 +49,25 @@ class Client(object):
         self.marker_detector.join()
 
     def run(self):
-        self.socket_client.connect()
+        # self.socket_client.connect()
         self.marker_detector.start()
-        while True:
-            try:
-                self.socket_client.send(self.make_dict(self.marker_detector.get_marker()))
-                sleep(1)
-                response = self.socket_client.recv()
-                if not response:
-                    logger.warning("No response. Exit...")
-                    self.close()
-                    break
-                logger.debug(response)
-            except socket.error as e:
-                logger.error("SocketClient: %s", e)
-                self.close()
-                break
-            except KeyboardInterrupt:
-                logger.warning("SocketClient: Closing...")
-                self.close()
-                break
+        print self.marker_detector.get_marker()
+        # self.make_dict(self.marker_detector.get_marker())
+        # while True:
+        #     try:
+        #         self.socket_client.send(self.make_dict(self.marker_detector.get_marker()))
+        #         sleep(1)
+        #         response = self.socket_client.recv()
+        #         if not response:
+        #             logger.warning("No response. Exit...")
+        #             self.close()
+        #             break
+        #         logger.debug(response)
+        #     except socket.error as e:
+        #         logger.error("SocketClient: %s", e)
+        #         self.close()
+        #         break
+        #     except KeyboardInterrupt:
+        #         logger.warning("SocketClient: Closing...")
+        #         self.close()
+        #         break
--- client/markerdetector.py
+++ client/markerdetector.py
@@ -1,5 +1,5 @@
 import os
-from threading import Thread, Lock, Event, Timer
+from threading import Thread, Event
 
 import cv2
 import numpy as np
@@ -14,7 +14,7 @@ from log import logger
 class MarkerDetector(Thread):
 
     def __init__(self):
-        super(MarkerDetector, self).__init__()
+        Thread.__init__(self)
         self.daemon = True
         self._stop_event = Event()
 
@@ -26,9 +26,9 @@ class MarkerDetector(Thread):
         self.aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_50)
         self.aruco_params = aruco.DetectorParameters_create()
 
-        self.id = None
-        self.rotation = None
-        self.translation = None
+        self.ids = None
+        self.rotations = None
+        self.translations = None
 
         # self.marker = {
         #     "id" : "",
@@ -38,12 +38,18 @@ class MarkerDetector(Thread):
 
         self._calibrate()
 
+
+
     def run(self):
+        print "RUN"
         captured = cv2.VideoCapture(0)
+        print "RUN1"
+
+
         while not self.stopped():
             # make it like in a good old movie
             gray = self._achromatise(captured)
-            self.id, self.rotation, self.translation = self._detect(gray)
+            self.ids, self.rotations, self.translations = self._detect(gray)
 
     def _calibrate(self):
         try:
@@ -71,12 +77,12 @@ class MarkerDetector(Thread):
         corners, ids, rejectedImgPoints = aruco.detectMarkers(
             gray, self.aruco_dict, parameters=self.aruco_params)
         if type(ids) is np.ndarray:  # if aruco marker detected
-            self._log_markers(ids)
+            # self._log_markers(ids)
 
             rvec, tvec, _objPoints = aruco.estimatePoseSingleMarkers(
                 corners, self.MARKER_SIZE, self.camera_matrix,
                 self.dist_coeffs)
-            return ids[0], rvec[0][0], tvec[0][0]
+            return ids, rvec, tvec
         else:
             return None, None, None
 
@@ -87,7 +93,7 @@ class MarkerDetector(Thread):
         logger.info("Marker(s) id:%s", marker_string)
 
     def get_marker(self):
-        return self.id, self.rotation, self.translation
+        return self.ids, self.rotations, self.translations
 
     def stop(self):
         self._stop_event.set()
