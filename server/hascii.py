408a43f0af955d3e11cd27b75f553cdf0b4495dd
stsdc <s.dac@yandex.ru>
2017-11-20 13:23:37 +0100

    more descriptive decode error

--- server/hascii.py
+++ server/hascii.py
@@ -28,7 +28,7 @@ def decode(data):
     try:
         return int(hexed, 16)
     except ValueError as e:
-        logger.error("HAscii: Decode: %s", e)
+        logger.error("HAscii: Trying to decode: [%s], %s", data, e)
 
 # encoded8 = encode8(100)
 # encoded3 = encode3(1234)
