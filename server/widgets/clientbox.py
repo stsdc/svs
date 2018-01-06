feeb28b3783520f6ee7e83e9b29f11e7105608c4
stsdc <s.dac@yandex.ru>
2018-01-06 16:37:32 +0100

    convert to euler angles

--- server/widgets/clientbox.py
+++ server/widgets/clientbox.py
@@ -1,9 +1,9 @@
 from box import Box
-import os
-
-
+import cv2
+import numpy as np
+from log import logger
 # import threading
-
+import math
 
 class ClientBox(Box):
     def __init__(self, h, w, y, x, title="Client"):
@@ -60,7 +60,7 @@ class ClientBox(Box):
         if data:
             tranx = round(data[0])
             trany = round(data[1])
-            tranz = round(data[2]) + 20
+            tranz = round(data[2])
 
             self.add(pos_y + 1, pos_x, "%19d" % tranx)
             self.add(pos_y + 2, pos_x, "%19d" % trany)
@@ -76,9 +76,12 @@ class ClientBox(Box):
         self.add(pos_y + 3, 1, "Z:")
         pos_x = pos_x + 4
 
-        rotx = round(data[0] * 180 / 3.14)
-        roty = round(data[1] * 180 / 3.14)
-        rotz = round(data[2] * 180 / 3.14)
+        R = cv2.Rodrigues(np.array(data))[0]
+        euler = rotationMatrixToEulerAngles(R)
+
+        rotx = round(euler[0] * 180 / 3.14)
+        roty = round(euler[1] * 180 / 3.14)
+        rotz = round(euler[2] * 180 / 3.14)
 
         if data:
             self.add(pos_y + 1, pos_x, "%19d" % self.rot_lol(rotx))
@@ -102,4 +105,35 @@ class ClientBox(Box):
     def rot_lol(self, rotx):
         if rotx < 0:
             return 180 + rotx
-        return rotx
+        return rotx - 180
+
+
+# Checks if a matrix is a valid rotation matrix.
+def isRotationMatrix(R):
+    Rt = np.transpose(R)
+    shouldBeIdentity = np.dot(Rt, R)
+    I = np.identity(3, dtype=R.dtype)
+    n = np.linalg.norm(I - shouldBeIdentity)
+    return n < 1e-6
+
+
+# Calculates rotation matrix to euler angles
+# The result is the same as MATLAB except the order
+# of the euler angles ( x and z are swapped ).
+def rotationMatrixToEulerAngles(R):
+    assert (isRotationMatrix(R))
+
+    sy = math.sqrt(R[0, 0] * R[0, 0] + R[1, 0] * R[1, 0])
+
+    singular = sy < 1e-6
+
+    if not singular:
+        x = math.atan2(R[2, 1], R[2, 2])
+        y = math.atan2(-R[2, 0], sy)
+        z = math.atan2(R[1, 0], R[0, 0])
+    else:
+        x = math.atan2(-R[1, 2], R[1, 1])
+        y = math.atan2(-R[2, 0], sy)
+        z = 0
+
+    return np.array([x, y, z])
